8113579e20149b565665b477832cb9e2ec9cd372
Boris Chigirev <borischigirev@MacBook-Pro-Boris.local>
2023-07-11 16:34:50 +0300

    add cli.js and add askNameAndGreet

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
 console.log('Welcome to the Brain Games!');
+
+import { askNameAndGreet } from '/Users/borischigirev/Desktop/Hexlet/frontend-project-44/src/cli.js'
+
+askNameAndGreet();
+
